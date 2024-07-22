2ee53a0770f256a6f50a471898b74d2cf6383096
Hassan Arshad <143306294+hassan34234234@users.noreply.github.com>
2024-07-22 21:54:30 +0500

    Update upload_to_s3.py

--- Python script/upload_to_s3.py
+++ Python script/upload_to_s3.py
@@ -1,14 +1,12 @@
 import boto3
 
-# Replace with your AWS credentials and bucket info
 BUCKET_NAME = "my-unique-bucket-2024-07-15"
-FILE_NAME = "Superstore.csv"  # Replace with your actual file name
-FILE_PATH = "C:/Users/HASSAN/Downloads/Data Engineering Project/Data-Engineering-Project/Python script/Superstore.csv"  # Replace with your actual path
+FILE_NAME = "Superstore.csv"  
+FILE_PATH = "C:/Users/HASSAN/Downloads/Data Engineering Project/Data-Engineering-Project/Python script/Superstore.csv"  
 
-# Create an S3 client
+
 s3 = boto3.client('s3')
 
-# Upload the file
 try:
   s3.upload_file(FILE_PATH, BUCKET_NAME, FILE_NAME)
   print(f"Successfully uploaded {FILE_NAME} to S3 bucket {BUCKET_NAME}")
