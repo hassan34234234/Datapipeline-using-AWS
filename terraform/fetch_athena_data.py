53f77ea5555eaf63ce0ea122ca1a18ce51709b53
Hassan Arshad <143306294+hassan34234234@users.noreply.github.com>
2024-07-22 21:55:59 +0500

    Update fetch_athena_data.py

--- terraform/fetch_athena_data.py
+++ terraform/fetch_athena_data.py
@@ -1,20 +1,17 @@
 import awswrangler as wr
 import pandas as pd
 
-# Replace with your actual S3 bucket and database details
 s3_output = "s3://my-unique-bucket-2024-07-15/"
 database = "new_database"
 table = "superstore_table"
 
-# Define the query
-#query = f"SELECT * FROM {table};"
 
-# Fetch data from Athena
+query = f"SELECT * FROM {table};"
+
 df = wr.athena.read_sql_query(
     sql=query,
     database=database,
     s3_output=s3_output
 )
 
-# Save the data to a CSV file
 df.to_csv("superstore_data.csv", index=False)
